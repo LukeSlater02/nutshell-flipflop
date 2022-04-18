0c4266e59fa4848d8f8c345b30b45289c8bf59ad
acheatham87 <acheatham87@gmail.com>
2022-04-18 09:38:12 -0500

    added styling to friends page

--- src/components/friends/FriendCard.js
+++ src/components/friends/FriendCard.js
@@ -1,18 +1,17 @@
 //Andrew Cheatham 
-//Creates component FriendCard which allows each individual friends information to be displayed onto the dom using JSX
-//Added Remove Friend Button 
 
 import react from "react";
-
 import "./FriendCard.css"
 
 export const FriendCard = ({friend, handleDeleteFriend}) => {
+    //creates the component FriendCard to display the information of each individual friend from the fetch call getAllFriends
+
     return(
         <div className="card">
             <div className="card-content">
                 <h3 className="card-friend-name"> {friend.user.name} </h3>
                 <p className="card-friend-email"> {friend.user.email} </p>
-                <button type="button" className="card-friend-delete" onClick={() => handleDeleteFriend(friend.id)}> Remove Friend </button>
+                <button  className="card-friend-delete" onClick={() => handleDeleteFriend(friend.id)}> Remove Friend </button>
             </div>
         </div>
     )
