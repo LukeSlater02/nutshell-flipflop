67a9f3b5a1ae29ac872ff831a98232c645a3f5f8
Hulikner <hulikner@gmail.com>
2022-04-19 13:59:38 -0500

    checkmark functionality on taskList

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./TaskCard.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faXmark} from '@fortawesome/free-solid-svg-icons'
 import { epochDateConverter } from '../util/epochDateConverter';
 import { format } from 'date-fns';
+import { updateTask } from '../../modules/TaskManager';
 
 
 export const TaskCard = ({ task, handleDeleteTask }) => {
 
+  const [ tasks, setTask ] = useState(false)
+
   const formattedDate = task?.deadline && epochDateConverter(task.deadline, 'eee. MMM do')
 
-
+  const handleCompletionChange = (task) => {
+    task.isCompleted === true ? task.isCompleted = false : task.isCompleted = true ;
+    if(task.completeDate === 0){
+      task.completeDate = new Date().getTime()/1000
+    }
+    if(task.isCompleted === false && task.completeDate == 1){
+      task.completeDate = 0
+    }
+    updateTask(task)
+    .then(task.isCompleted === false ? setTask(true) : setTask(false))
+  }
 
 
   return (
@@ -23,9 +36,10 @@ export const TaskCard = ({ task, handleDeleteTask }) => {
           {task.name}
         </span>
         <span className='card-date'>{formattedDate}</span>
-        <span className='card-status'>{task.isCompleted === false || 0 ? <span className='x'><FontAwesomeIcon icon={faXmark} /></span>: <span className='check'><FontAwesomeIcon icon={faCheck} /></span>}</span>
     
       </Link>
+        {task.isCompleted ? <button onClick={()=> handleCompletionChange(task)} className='card-status-done'><FontAwesomeIcon icon={faCheck} /></button> :
+        <button onClick={()=> handleCompletionChange(task)} className='card-status-not-done'><FontAwesomeIcon icon={faCheck} /></button>}
 
         </div>
    
